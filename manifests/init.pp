4017baf0a6239581aff5e1b8d7dce2066a4450eb
Thodoris Sotiropoulos <theosotr@aueb.gr>
2019-02-04 17:58:43 +0200

    Add missing ordering relationship

--- manifests/init.pp
+++ manifests/init.pp
@@ -49,7 +49,8 @@ class influxdbrelay (
 
   file { $dirs:
     ensure => 'directory',
-    mode   => '0755'
+    mode   => '0755',
+    before => Exec['post-install'],
   }
 
   file { '/etc/environment':
