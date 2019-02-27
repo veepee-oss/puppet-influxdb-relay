f15242f3651fae7f67f3b8de352149ae44c8b2af
Camille TRUCHOT <ctruchot@vente-privee.com>
2019-02-27 17:20:54 +0100

    fixes for new configuration

--- manifests/params.pp
+++ manifests/params.pp
@@ -18,25 +18,34 @@ class influxdbrelay::params {
   ]
 
   # http params
-  $http_name      = 'http-relay'
-  $http_bind_addr = '127.0.0.1'
-  $http_bind_port = '9096'
-  $http_ssl       = false
-  $http_ssl_cert  = ''
-  $http_outputs   = [
+  $http_name              = 'http-relay'
+  $http_bind_addr         = '127.0.0.1'
+  $http_bind_port         = '9096'
+  $http_health_timeout_ms = '2000'
+  $http_ssl               = false
+  $http_ssl_cert          = ''
+  $http_outputs           = [
     {
-      'name'    => 'local1',
-      'schema'  => 'http',
-      'host'    => '127.0.0.1',
-      'port'    => '8086',
-      'timeout' => '10s'
+      'name'       => 'local1',
+      'schema'     => 'http',
+      'host'       => '127.0.0.1',
+      'port'       => '8086',
+      'timeout'    => '10s',
+      'write'      => '/write',
+      'write_prom' => '/write_prom',
+      'ping'       => '/ping',
+      'query'      => '/query'
     },
     {
-      'name'    => 'local2',
-      'schema'  => 'http',
-      'host'    => '127.0.0.1',
-      'port'    => '7086',
-      'timeout' => '10s'
+      'name'       => 'local2',
+      'schema'     => 'http',
+      'host'       => '127.0.0.1',
+      'port'       => '8086',
+      'timeout'    => '10s',
+      'write'      => '/write',
+      'write_prom' => '/write_prom',
+      'ping'       => '/ping',
+      'query'      => '/query'
     }
   ]
   $udp_name       = 'udp-relay'
